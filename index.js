96e5fbc74fb993d47fe76eecfd062873b337cda4
TerraZero <terrazerocore@gmail.com>
2021-03-10 10:53:44 +0100

    plugin

--- index.js
+++ index.js
@@ -1,8 +1,30 @@
-module.exports = class PenclConfig {
+const PenclPlugin = require('pencl-base/src/Boot/PenclPlugin');
+const ConfigManager = require('./src/ConfigManager');
 
-  
-  static get ConfigManager() {
-    return require('./src/ConfigManager');
+class PenclConfig extends PenclPlugin {
+
+  static get name() {
+    return 'config';
+  }
+
+  static get config() {
+    return {
+      path: '~/data/configs',
+    };
+  }
+
+  constructor() {
+    super();
+    this._manager = null;
+  }
+
+  get manager() {
+    if (this._manager === null) {
+      this._manager = new ConfigManager(this.config.path);
+    }
+    return this._manager;
   }
 
 }
+
+module.exports = new PenclConfig();
